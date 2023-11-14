29979039083cf1e177efdb00dbd92c37c1127ca0
Alba Alonso <alba.alonso@ikasle.egibide.org>
2023-11-14 20:41:31 +0100

    generate javadoc

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Cliente"},{"l":"Cliente.Vista"},{"l":"Common.Mangers"},{"l":"Common.Message"},{"l":"Common.Validar"},{"l":"Servidor"}];updateSearchResults();
